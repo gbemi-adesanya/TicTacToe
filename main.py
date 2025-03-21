9eeb93e1acc5a12df706c1fa11a5ff71afd8315b
gbemi-adesanya <143091224+gbemi-adesanya@users.noreply.github.com>
2025-03-20 23:53:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def player_move(grid, current_player, empty_cells):
     :param empty_cells: list, unoccupied cells as tuples
     """
     while True:
-        if len(empty_cells) > 1:
+        if len(empty_cells) > 1:            # moves can still be made, auto-play not required
             try:
                 row, col = map(int, input(f"Player {current_player}, where do you want to place your choice (row col)? e.g. 1 1: ").split())
                 grid.modify_cell(current_player, row, col)
@@ -20,30 +20,19 @@ def player_move(grid, current_player, empty_cells):
         else:  # auto-play
             row, col = empty_cells[0]
             grid.modify_cell(current_player, row, col)
-            # print("DPS: Autoplay engaged")
             break
 
 def game_play():
     play_again = "y"
     while play_again == "y":
         print("Do you want to play against a friend (F) or against the computer (C)?")
-        mode = (input("Choose game mode: F or C? ")).upper()
+        mode = (input("Choose game mode: ")).upper()
         while mode not in ["F", "C"]:
             print("Invalid input.", end=" ")
             mode = (input("Choose game mode: F or C? ")).upper()
         print()
 
-
-        while True:
-            try:
-                n = int(input("Input the size of the grid (at least 3): "))
-                if n < 3:
-                    print("Grid size must be at least 3.\n")
-                else:
-                    break
-            except ValueError:
-                print("Invalid input. Please enter a valid integer.")
-
+        n = 3       # grid size
         current_player = "X"
         grid = Grid(n)
         print(grid)
@@ -68,6 +57,7 @@ def game_play():
 
             current_player = "X" if current_player == "O" else "O"          # alternate players
 
+        print()
         play_again = (input("Do you want to play again? yes (y)/ no (n): ")).lower()
         print()
 
